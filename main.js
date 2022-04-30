7206ff54aa7a2e4b85726eb4c15888e99d36b613
Marcy Roca <peipr@peiphy.xyz>
2022-04-30 17:21:02 +0200

    Fixed bug

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ console.log("Preparing packages");
 const Scraper = require('images-scraper');
 const {writeFile} = require('fs');
 const path = './json/test.json';
+var sleep = require('sleep');
 ////////////////////////////Get image from Google API///////////////////////////////////////////////////////////////////////////////
 const limit = 50
 const google = new Scraper({ puppeteer:{headless:false},});
@@ -21,12 +22,13 @@ const nthElement = (arr, n = 0) => (n > 0 ? arr.slice(n, n + 1) : arr.slice(n))[
 console.log(i);
 let img2 = (nthElement(results, i));
 console.log(img2.url);
-        sleep(1000).then(() => {
-            let curltime = cmd.runSync(`curl -s "https://api-us.restb.ai/vision/v2/multipredict" \ -d model_id="re_roomtype_global_v2,re_features_v3,re_appliances_v2" \-d image_url=${img2.url} \-d client_key="78b4fdd3020ebcd8fffadf001377b0fb9138a677f4f8b92131e7a7c18c93d468" > ./${i}.json`);
-            let dl = cmd.runSync(`curl -s -o "${i}.jpg" "${img2.url}"`);
-            console.log(dl);
-            console.log(curltime);
-        })
+let n = 1;
+function sleep(n) {
+let curltime = cmd.runSync(`curl -s "https://api-us.restb.ai/vision/v2/multipredict" \ -d model_id="re_roomtype_global_v2,re_features_v3,re_appliances_v2" \-d image_url=${img2.url} \-d client_key="78b4fdd3020ebcd8fffadf001377b0fb9138a677f4f8b92131e7a7c18c93d468" > ./${i}.json`);
+let dl = cmd.runSync(`curl -s -o "${i}.jpg" "${img2.url}"`);
+console.log(dl);
+console.log(curltime);}
+
 
 }})();
 
